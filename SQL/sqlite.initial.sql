3b0f48c58fc119a2c90ab72fa1da9060910c6299
Gene Hawkins <texxasrulez@yahoo.com>
2020-08-12 05:25:10 -0700

    Update sqlite.initial.sql

--- SQL/sqlite.initial.sql
+++ SQL/sqlite.initial.sql
@@ -5,26 +5,44 @@
  *
  * @licence GNU AGPL
  */
+ 
+-- import to SQLite by running: sqlite3.exe db.sqlite3 -init sqlite.sql
+
+PRAGMA journal_mode = MEMORY;
+PRAGMA synchronous = OFF;
+PRAGMA foreign_keys = OFF;
+PRAGMA ignore_check_constraints = OFF;
+PRAGMA auto_vacuum = NONE;
+PRAGMA secure_delete = OFF;
+BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS 'additional_imap' (
-  'id' INTEGER NOT NULL PRIMARY KEY ASC,
-  'user_id' int(10) NOT NULL,
-  'iid' int(10) NOT NULL,
-  'username' varchar(256) DEFAULT NULL,
-  'password' text,
-  'server' varchar(256) DEFAULT NULL,
-  'enabled' int(1) NOT NULL DEFAULT '0',
-  'label' text,
-  'preferences' text,
-  CONSTRAINT 'additional_imap_ibfk_1' FOREIGN KEY ('user_id') REFERENCES 'users'
-    ('user_id') ON DELETE CASCADE ON UPDATE CASCADE
+'id' INTEGER NOT NULL PRIMARY KEY ASC,
+'user_id' INTEGER NOT NULL,
+'iid' INTEGER NOT NULL,
+'username' TEXT DEFAULT NULL,
+'password' text,
+'server' TEXT DEFAULT NULL,
+'enabled' INTEGER NOT NULL DEFAULT '0',
+'label' text,
+'preferences' text,
+FOREIGN KEY ('user_id') REFERENCES 'users'
+('user_id') ON DELETE CASCADE ON UPDATE CASCADE
 );
-
 CREATE TABLE IF NOT EXISTS 'additional_imap_hosts' (
-  'id' INTEGER NOT NULL PRIMARY KEY ASC,
-  'domain' varchar(255) NOT NULL,
-  'host' varchar(255) DEFAULT NULL,
-  'ts' datetime NOT NULL
+'id' INTEGER NOT NULL PRIMARY KEY ASC,
+'domain' TEXT NOT NULL,
+'host' TEXT DEFAULT NULL,
+'ts' datetime NOT NULL
 );
-
 CREATE INDEX additional_imap_iid ON 'additional_imap'('iid');
+
+
+
+
+
+COMMIT;
+PRAGMA ignore_check_constraints = ON;
+PRAGMA foreign_keys = ON;
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;
